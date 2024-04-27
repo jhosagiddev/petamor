e58aa2e4f97c91fabb8beeaccdb33555cba02438
Jhonny Pirela <jhosagid7@gmail.com>
2024-04-27 09:44:05 -0400

    Create route group for Customers and Pets

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('pets', PetController::class);
+});
